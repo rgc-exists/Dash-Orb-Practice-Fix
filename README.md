875ec0f9a5fe95af3d97c5d5ee9467c741fde705
rgc-exists <103580754+rgc-exists@users.noreply.github.com>
2025-06-20 13:47:40 -0400

    Finish v1.0.0 (in theory)

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
-# DashOrbPracticeFix
-This is where she makes a mod.
-
-<img src="logo.png" width="150" alt="the mod's logo" />
-
-TODO: Commit bindings for PlayerObject::saveToCheckpoint so I can use it.
+# Dash Orb Practice Fix
+A mod that makes it so you can place checkpoints while holding a dash orb, and lets you continue holding dash orbs after you respawn from those checkpoints.
+## "Affect Platformer Checkpoints" Setting
+This mod has a setting that, when enabled, makes it so the dash orb persistence applies to the platformer-only "checkpoint" objects in normal mode, not just checkpoints from practice mode.  
+This setting is off by default.
+# Incompatibilities
+This mod is incompatible with the "Platformer Saves" and "Persistence API" mods due to internal reasons. If someone wants to make a pull request to fix this, be my guest.
--- src/main.cpp
+++ src/main.cpp
@@ -17,13 +17,17 @@ class $modify(PlayLayer) {
 		player_CP->m_dashY = player->m_dashY;
 		player_CP->m_dashRingObject = player->m_dashRing;
 		player_CP->m_dashAngle = player->m_dashAngle;
-		player_CP->m_startTime = player->m_dashStartTime;
+		player_CP->m_dashStartTime = player->m_dashStartTime;
 	}
 
 	CheckpointObject* markCheckpoint() {
 
 		PlayerObject* player1 = GJBaseGameLayer::get()->m_player1;
 		PlayerObject* player2 = GJBaseGameLayer::get()->m_player2;
+
+		if (player1 == nullptr || player1->m_isDead) {
+			return nullptr;
+		}
 
 		bool p1_dashing = player1->m_isDashing;
 		player1->m_isDashing = false; // Workaround so the game actually places the checkpoint,
@@ -52,14 +56,19 @@ int justRespawnedCP = -1;
 class $modify(PlayerObject) {
 
 	void dashFromCP() {
+		bool hasNoEffects_cache = m_dashRing->m_hasNoEffects;
+		m_dashRing->m_hasNoEffects = true;
 		startDashing(m_dashRing);
 		m_flashTime = 0;
+		m_dashRing->m_hasNoEffects = hasNoEffects_cache;
 	}
 
 	void update(float p0) {
 		PlayerObject::update(p0);
 		
-		justRespawnedCP--;
+		if (!m_isSecondPlayer) {
+			justRespawnedCP--;
+		}
 		if (m_holdingButtons[1] && justRespawnedCP == 0) {
 			dashFromCP();
 		}
@@ -67,14 +76,21 @@ class $modify(PlayerObject) {
 
 	void loadFromCheckpoint(PlayerCheckpoint * checkpoint) {
 
-		m_isDashing = checkpoint->m_dashing;
-		m_dashX = checkpoint->m_dashX;
-		m_dashY = checkpoint->m_dashY;
-		m_dashRing = checkpoint->m_dashRingObject;
-		m_dashAngle = checkpoint->m_dashAngle;
-		m_dashStartTime = checkpoint->m_startTime;
+		bool affectPlat = Mod::get()->getSettingValue<bool>("affect-platformer-checkpoints");
+		if (checkpoint->m_platformerCheckpoint && !affectPlat) {
+			PlayerObject::loadFromCheckpoint(checkpoint);
+			return;
+		}
 
-		justRespawnedCP = 2;
+		m_isDashing = checkpoint->m_dashing;
+		if (m_isDashing) {
+			m_dashX = checkpoint->m_dashX;
+			m_dashY = checkpoint->m_dashY;
+			m_dashRing = checkpoint->m_dashRingObject;
+			m_dashAngle = checkpoint->m_dashAngle;
+			m_dashStartTime = checkpoint->m_dashStartTime;
+			justRespawnedCP = 2;
+		}
 		PlayerObject::loadFromCheckpoint(checkpoint);
 
 		if (m_isDashing) {
@@ -83,10 +99,8 @@ class $modify(PlayerObject) {
 	}
 
 	bool releaseButton(PlayerButton button) {
-		log::debug("releaseButton called.");
 		if (justRespawnedCP >= 0) {
 			bool isDashing_cache = m_isDashing;
-			log::debug("isDashing_cache: {}", isDashing_cache);
 			m_isDashing = false;
 
 			bool jumpBuffered_cache = m_jumpBuffered;
@@ -94,14 +108,12 @@ class $modify(PlayerObject) {
 			bool stateRingJump2_cache = m_stateRingJump2;
 
 			if (!isDashing_cache || button != PlayerButton::Jump) {
-				log::debug("calling OG function...", isDashing_cache);
 				if (!PlayerObject::releaseButton(button)) {
 					m_isDashing = isDashing_cache;
 					return false;
 				}
 			}
 			else {
-				log::debug("NO NEED to call OG function.", isDashing_cache);
 				m_holdingButtons[1] = true;
 			}
 
@@ -109,6 +121,15 @@ class $modify(PlayerObject) {
 			bool m_jumpBuffered = jumpBuffered_cache;
 			bool m_stateRingJump = stateRingJump_cache;
 			bool m_stateRingJump2 = stateRingJump2_cache;
+
+			if (m_isDashing) {
+				PlayerObject::updateDashArt();
+				m_dashParticles->setVisible(false);
+				for (int i = 0; i < m_dashSpritesContainer->getChildrenCount(); i++) {
+					CCSprite* sprite = (CCSprite*)m_dashSpritesContainer->getChildren()->objectAtIndex(i);
+					sprite->setVisible(false);
+				}
+			}
 		}
 		else {
 			if (!PlayerObject::releaseButton(button)) {
@@ -117,127 +138,4 @@ class $modify(PlayerObject) {
 		}
 		return true;
 	}
-
-	void stopDashing() {
-		log::debug("stopDashing called.");
-		log::debug("justRespawnedCP: {}", justRespawnedCP);
-
-		if (justRespawnedCP < 0) {
-			return PlayerObject::stopDashing();
-		}
-		justRespawnedCP -= 1;
-	}
 };
-
-/*
-class $modify(PCheckpointHook, PlayerCheckpoint) {
-	struct Fields {
-		DashRingObject* m_dashRing;
-		bool m_isDashing;
-		double m_dashX;
-		double m_dashY;
-		double m_dashAngle;
-		double m_dashStartTime;
-	};
-};
-
-class $modify(PLayerHook, PlayLayer) {
-
-	struct Fields {
-		int m_cpRespawnTimer = -1;
-		bool m_p1WasDashing = false;
-		bool m_p2WasDashing = false;
-	};
-
-	static void updatePlayerCP(PCheckpointHook * player_CP, PlayerObject * player) {
-		player_CP->m_fields->m_isDashing = player->m_isDashing;
-		player_CP->m_fields->m_dashX = player->m_dashX;
-		player_CP->m_fields->m_dashY = player->m_dashY;
-		player_CP->m_fields->m_dashRing = player->m_dashRing;
-		player_CP->m_fields->m_dashStartTime = player->m_dashStartTime;
-	}
-
-	void makePlayerDash(PlayerObject* player) {
-		player->startDashing(player->m_dashRing);
-		player->m_flashTime = 0;
-	}
-
-	virtual void postUpdate(float p0) {
-
-		PlayLayer::postUpdate(p0);
-
-		if (m_fields->m_cpRespawnTimer >= 0) {
-			if (m_fields->m_p1WasDashing) {
-				if (m_player1->m_holdingButtons[1]) {
-					makePlayerDash(m_player1);
-				}
-			}
-
-			if (m_fields->m_p2WasDashing && m_player2) {
-				if (m_player2->m_holdingButtons[1]) {
-					makePlayerDash(m_player2);
-				}
-			}
-		}
-		m_fields->m_cpRespawnTimer -= 1;
-	}
-
-	CheckpointObject* markCheckpoint() {
-
-		PlayerObject* player1 = GJBaseGameLayer::get()->m_player1;
-		PlayerObject* player2 = GJBaseGameLayer::get()->m_player2;
-
-		bool p1_dashing = player1->m_isDashing;
-		player1->m_isDashing = false; // Workaround so the game actually places the checkpoint,
-		bool p2_dashing = false;      // Since RobTop made it so you CAN'T PLACE CHECKPOINTS while dashing instead of fixing it. :|
-		if (player2) {				  
-			p2_dashing = player2->m_isDashing;
-			player2->m_isDashing = false; 
-		}
-
-		CheckpointObject* checkpoint = PlayLayer::markCheckpoint();
-
-		PCheckpointHook* player1_CP = static_cast<PCheckpointHook*>(checkpoint->m_player1Checkpoint);
-		player1->m_isDashing = p1_dashing;
-		updatePlayerCP(player1_CP, player1);
-
-		if (player2 && checkpoint->m_player2Checkpoint) {
-			player2->m_isDashing = p2_dashing;
-
-			PCheckpointHook* player2_CP = static_cast<PCheckpointHook*>(checkpoint->m_player2Checkpoint);
-			updatePlayerCP(player2_CP, player2);
-		}
-
-
-		return checkpoint;
-	}
-};
-
-class $modify(PlayerObject) {
-
-	void loadFromCheckpoint(PlayerCheckpoint* checkpoint) {
-
-		PLayerHook* playLayerHook = static_cast<PLayerHook*>(GameManager::get()->m_playLayer);
-		playLayerHook->m_fields->m_cpRespawnTimer = 1;
-
-		PCheckpointHook* player_CP = static_cast<PCheckpointHook*>(checkpoint);
-		bool wasDashing = player_CP->m_fields->m_isDashing;
-		if (m_isSecondPlayer) {
-			playLayerHook->m_fields->m_p2WasDashing = wasDashing;
-		}
-		else {
-			playLayerHook->m_fields->m_p1WasDashing = wasDashing;
-		}
-
-		PlayerObject::loadFromCheckpoint(checkpoint);
-
-		PCheckpointHook* cpHook = static_cast<PCheckpointHook*>(checkpoint);
-
-		m_isDashing = cpHook->m_fields->m_isDashing;
-		m_dashX = cpHook->m_fields->m_dashX;
-		m_dashY = cpHook->m_fields->m_dashY;
-		m_dashRing = cpHook->m_fields->m_dashRing;
-		m_dashStartTime = cpHook->m_fields->m_dashStartTime;
-	}
-};
-*/
